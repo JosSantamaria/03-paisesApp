e4f6462fbbdbbf7b1bdb48cbc15e46f5dee91302
Jos Santamaria <josetsantamaria@gmail.com>
2022-01-19 16:54:39 -0600

    placeholder agragado

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, Input } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
@@ -14,6 +14,8 @@ export class PaisInputComponent implements OnInit {
   @Output() onEnter: EventEmitter<string> = new EventEmitter(); //Creamos una salida ('@Outoput') que mandara los datos hacioa afuera del componente.
   @Output() onDebounce: EventEmitter<string> = new EventEmitter();//evento que ayudara con busquedas en 'real time'.
   
+  @Input() placeholder: string = '';
+
   termino:string = '';
   
   debouncer: Subject<string> = new Subject(); //El debouncer es un Observable especial
